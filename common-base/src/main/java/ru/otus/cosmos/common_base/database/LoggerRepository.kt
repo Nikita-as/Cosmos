d462313007cbb484703b1068e39e5bf8a5e655dd
Nikita Pakhomov <nikpakhomov@ozon.ru>
2025-01-14 20:00:54 +0300

    правки

--- common-base/src/main/java/ru/otus/cosmos/common_base/database/LoggerRepository.kt
+++
@@ -1,6 +0,0 @@
-package ru.otus.cosmos.common_base.database
-
-interface PhotoDayRepository {
-    suspend fun addInfo(title: String, image: String, info: String): Long
-    suspend fun clearDatabase()
-}
--- common-base/src/main/java/ru/otus/cosmos/common_base/database/PhotoDayDataSource.kt
+++
@@ -1,7 +0,0 @@
-package ru.otus.cosmos.common_base.database
-
-interface PhotoDayDataSource {
-    fun getAllInfo(): List<String>
-    fun addInfo(title: String, image: String, info: String): Long
-    fun clear()
-}
